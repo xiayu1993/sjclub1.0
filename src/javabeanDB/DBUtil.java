4da3714fe6ee0892789d2d73ab25c2ac2ff0b558
281878358@qq.com <XIAYUaaa1010>
2015-04-06 20:31:38 +0800

    尝试性的写了数据库操作类

--- src/javabeanDB/DBUtil.java
+++ src/javabeanDB/DBUtil.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 /**
  * 数据库操作
@@ -19,18 +20,36 @@ public class DBUtil {
 	private static String connUrl = "jdbc:sqlserver://sjclub.org:1433;databaseName=Club;";
 	private static String user = "sa";
 	private static String password = "xiayu1010";
+	private static Connection conn = null;
 	
 	/* 数据库查询，更新以及删除的参数 */
 	//Statement对象，用于执行不带参数的简单SQL语句
-	private Statement stmt = null;
+	private static Statement stmt = null;
 	//PreparedStatement对象，用于执行带参数的SQL语句
-	private PreparedStatement ps = null;
+	private static PreparedStatement ps = null;
 	//结果集
-	private ResultSet rs = null;
+	private static ResultSet rs = null;
+	
+	/*
+	 * 构造器
+	 */
+	public DBUtil(){
+		try{
+			//加载驱动
+			Class.forName(driver);
+			//数据库连接
+			conn = DriverManager.getConnection(connUrl, user, password);
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("#javabeanDB/DBUtil.java-1#");
+		}
+	}
+	
 	
 	/*
 	 * 数据库连接
-	 */
+	 * #javabeanDB/DBUtil.java-1#
+	 *
 	public static Connection getConnection(){
 		Connection conn = null;
 		try{
@@ -44,9 +63,12 @@ public class DBUtil {
 		}
 		return conn;
 	}
+	*
+	*/
 	
 	/*
 	 * 数据库断开
+	 * #javabeanDB/DBUtil.java-2#
 	 */
 	public static void closeConnection(Connection conn){
 		//判断conn是否为空
@@ -59,5 +81,42 @@ public class DBUtil {
 				System.out.println("#javabeanDB/DBUtil.java-2#");
 			}
 		}
+	}
+	
+	/*
+	 * 不带参数的数据库查询
+	 * #javabeanDB/DBUtil.java-3#
+	 */
+	public static ResultSet statementSql(String sql){
+		try{
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("#javabeanDB/DBUtil.java-3#");
+		}finally{
+			try{
+				rs.close();
+				stmt.close();
+				DBUtil.closeConnection(conn);
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+			
+		}
+		return rs;
+	}
+	
+	/*
+	 * 带参数的数据库查询
+	 * #javabeanDB/DBUtil.java-4#
+	 */
+	public static ResultSet preStatementSql(String sql, String... arrs){
+		try{
+		
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return rs;
 	}
 }
